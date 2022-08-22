026b2b4726867561a721478be1245da84e798452
kano-info <108403850+kano-info@users.noreply.github.com>
2022-08-22 12:49:50 +0800

    Update stream.js

--- js/stream.js
+++ js/stream.js
@@ -5,9 +5,9 @@ let slides = {records: []};
 function day_counter_render() {
     const today = new Date();
     const day_divider = 1000 * 60 * 60 * 24;
-    document.getElementById('day-counter').innerHTML = "<div>" +app_i18n.count_first_v(Math.floor((today - new Date("2019-08-01")) / day_divider)) + "</div>" +
-        "<div>" +app_i18n.count_first_y(Math.floor((today - new Date("2019-08-03")) / day_divider)) + "</div>" +
-        "<div>" +app_i18n.count_first_b(Math.floor((today - new Date("2019-08-17")) / day_divider)) + "</div>"
+    document.getElementById('day-counter').innerHTML = "<div>" +app_i18n.count_first_v(Math.floor((today - new Date("2015-05-20")) / day_divider)) + "</div>" +
+        "<div>" +app_i18n.count_first_y(Math.floor((today - new Date("2015-05-22")) / day_divider)) + "</div>" +
+        "<div>" +app_i18n.count_first_b(Math.floor((today - new Date("2019-08-03")) / day_divider)) + "</div>"
 }
 
 /* slides */
